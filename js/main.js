b5f9ff1322d6e7520547383fd3e9107f04e8fefb
LauraKo <kovarskyte@yahoo.com>
2020-11-24 20:48:36 +0200

    Rendering initial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log('JS is running...')
+// import {pvz sum} from './'; import {} from './'
+// kitame faile export {pvz sum}
+// treciame faile export {}
+
+// Duomenys
+
+
+// Logika
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+
+// // Apjungimas
+// console.log(sum(4, 7));
+
+import { renderSocials } from "./components/renderSocials.js";
+import { socialsData } from "./data/socialsData.js";
+
+renderSocials(socialsData);
