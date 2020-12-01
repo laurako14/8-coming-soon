b0fe2f05596e9547c51ff1544bbbeb844f0a155b
LauraKo <kovarskyte@yahoo.com>
2020-12-01 11:35:31 +0200

    Clock running with js

--- js/components/clock/renderClock.js
+++ js/components/clock/renderClock.js
@@ -1,3 +1,4 @@
+import { countTimeDiff } from "./countTimeDiff.js";
 /**
  * Generuoja statini laikrodi, kuris rodo, kiek liko laiko iki artimiausiu Naujuju Metu. 
  * @param {string} selector CSS taisykle, kaip rasti vieta, kur bus generuojamas laikrodzio HTML turinys.
@@ -19,33 +20,31 @@ function renderClock(selector) {
         return false;
     }
 
-    const date = new Date();
-    const currentYear = date.getFullYear(); 
-    const newYear = currentYear + 1;
-    const newYearDate = `${newYear}-01-01 00:00:00`;
-    const newYearObject = new Date(newYearDate);
-    const newYearMiliseconds = newYearObject.getTime();
-    const currentTimeMiliseconds = date.getTime();
-    let timeLeft = newYearMiliseconds - currentTimeMiliseconds;
+    const time = countTimeDiff();
 
-    const days = Math.floor(timeLeft / 1000 / 60 / 60 / 24);
-    timeLeft -= days * 1000 * 60 * 60 * 24;
-    const hours = Math.floor(timeLeft / 1000 / 60 / 60);
-    timeLeft -= hours * 1000 * 60 * 60;
-    const minutes = Math.floor(timeLeft / 1000 / 60);
-    timeLeft -= minutes * 1000 * 60
-    const seconds = Math.floor(timeLeft / 1000);
-
-    const HTML = `<div class="numbers days">${days}</div>
-            <div class="numbers hours">${hours}</div>
-            <div class="numbers minutes">${minutes}</div>
-            <div class="numbers seconds">${seconds}</div>
+    const HTML = `<div class="numbers days">${time.days}</div>
+            <div class="numbers hours">${time.hours}</div>
+            <div class="numbers minutes">${time.minutes}</div>
+            <div class="numbers seconds">${time.seconds}</div>
             <div class="time ds">days</div>
             <div class="time hrs">hours</div>
             <div class="time min">minutes</div>
             <div class="time scs">seconds</div>`;
 
     DOM.innerHTML = HTML;
+
+    const timesDOM = DOM.querySelectorAll('.numbers'); 
+    
+    let timePassed = 0;
+
+    setInterval(() => {
+        const time = countTimeDiff();
+        timesDOM[0].innerHTML = time.days;
+        timesDOM[1].innerHTML = time.hours;
+        timesDOM[2].innerHTML = time.minutes;
+        timesDOM[3].innerHTML = time.seconds;
+    }, 1000);
+
     return true;
 }
 
